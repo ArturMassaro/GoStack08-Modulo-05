407b02bde9e086b4c8ebd34f824a6dd6bfc22698
Artur Massaro <arturmassaro3@hotmail.com>
2019-09-09 23:25:48 -0300

    Updates

---
+++ precache-manifest.d7e0e0b311ffed2fcf1a7d24fd0c6b72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869f7833bcba46dc269be6aecaf3479d",
+    "url": "/GoStack08-Modulo-05/index.html"
+  },
+  {
+    "revision": "f91209a69f71d2f8a830",
+    "url": "/GoStack08-Modulo-05/static/js/2.de78b8cf.chunk.js"
+  },
+  {
+    "revision": "b857e9253e7e142f4414",
+    "url": "/GoStack08-Modulo-05/static/js/main.11c7ed61.chunk.js"
+  },
+  {
+    "revision": "2f36b818923df839a0d3",
+    "url": "/GoStack08-Modulo-05/static/js/runtime~main.63f760f3.js"
+  }
+]);
